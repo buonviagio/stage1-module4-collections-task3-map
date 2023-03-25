f416cb80b108e134e96bdfb72c6ea03aa3b80135
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-25 16:42:09 +0200

    My second commit Stage1 Module#Collection Task#Map

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -3,14 +3,19 @@ package com.epam.mjc.collections.map;
 import java.util.*;
 
 public class KeyValueSwapper {
+
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> map = new LinkedHashMap<>();
         String strValue;
         int numberValue;
+
         for (Map.Entry<Integer, String> m : sourceMap.entrySet()) {
             strValue = m.getValue();
             numberValue = m.getKey();
-            if (!map.containsValue(strValue)) {
+            if (!map.containsKey(strValue)) {
+                map.put(strValue, numberValue);
+            }
+            if (map.containsKey(strValue) && map.get(strValue) > numberValue) {
                 map.put(strValue, numberValue);
             }
         }
